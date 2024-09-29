b76918158fd5902cdbfa59c23a3fa5ad84119785
filter00 <alienorigin36940@gmail.com>
2024-09-30 01:58:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/JishuDeveloper/Ultra-Forward-Bot JishuDeveloper/Ultra-Forward-Bot
+  git clone https://github.com/filter00/Ultra-Forward-Bot filter00/Ultra-Forward-Bot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/JishuDeveloper/Ultra-Forward-Bot -b $BRANCH /Ultra-Forward-Bot
+  git clone https://github.com/filter00/Ultra-Forward-Bot -b $BRANCH /Ultra-Forward-Bot
 fi
 cd JishuDeveloper/Ultra-Forward-Bot
 pip3 install -U -r requirements.txt
